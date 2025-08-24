db6ffcb48aff715533e6c0faca8a491c372775ac
Sagiri777 <guoziming08@126.com>
2025-08-24 12:15:02 +0800

    Fix ENCODED_DEFAULT string

--- src/launcher/openbachelorc/const.py
+++ src/launcher/openbachelorc/const.py
@@ -2,7 +2,7 @@ import base64
 from .config import config
 
 ENCODED_GADGET = "YW5pbWUucHZ6Lm9ubGluZQ=="
-ENCODED_DEFAULT = "Y29tLmh5cGVyZ3J5cGguYXJrbmlnaHRz
+ENCODED_DEFAULT = "Y29tLmh5cGVyZ3J5cGguYXJrbmlnaHRz"
 
 if config["use_gadget"]:
     PACKAGE_NAME = base64.b64decode(ENCODED_GADGET).decode('utf-8')
